3df16547c81736202972284e460aaee178344870
Thitiwas <champtnpc@gmail.com>
2016-12-11 11:36:08 +0700

    botweatherapi8

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ app.post('/webhook/', function (req, res) {
       }, function(error, response, body) {
         try {
           var condition = body.main;
-          var namelocation = body.name;
-          sendTextMessage(sender, "Today is " + condition.temp + "Celsius in " + namelocation.name);
+          sendTextMessage(sender, "Today is " + condition.temp + "Celsius in " + location);
         } catch(err) {
           console.error('error caught', err);
           sendTextMessage(sender, "There was an error.");
