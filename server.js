6451a48e14ae92d7a538c1b1985e0241e3fe5d71
Thitiwas <champtnpc@gmail.com>
2016-12-11 11:41:48 +0700

    botweatherapi10

--- server.js
+++ server.js
@@ -42,6 +42,10 @@ app.post('/webhook/', function (req, res) {
         sendGenericMessage(sender)
         continue
       }
+      // let textEcho = text.split(' ')
+      // sendTextMessage(sender, parseInt(textEcho[0]) + parseInt(textEcho[1]))
+         sendTextMessage(sender, 'Welcome to Weather')
+         sendTextMessage(sender, 'please enter your City')
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
