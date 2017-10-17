a6d2d4c93be2eaa790866245fb1b8461b3202c9c
Thitiwas <champtnpc@gmail.com>
2017-10-17 08:49:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/webhook/', function (req, res) {
       }, function(error, response, body) {
         try {
           var condition = body.main;
-          sendTextMessage(hello);
+          sendTextMessage('hello');
         } catch(err) {
           console.error('error caught', err);
           sendTextMessage(sender, "There was an error.");
